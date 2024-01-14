e9f793534a325fae971bd6883894547e61ba981b
Yu Li <liyu1981@gmail.com>
2024-01-14 20:07:45 +1100

    genearted kcov/docs

---
+++ gh-page/docs/data-modules.js
@@ -0,0 +1,1 @@
+var modules =[{"name":"zcmd","file":0,"main":66,"table":{"std":1,"zcmd":0}},{"name":"std","file":0,"main":67,"table":{}}];
---
+++ gh-page/kcov/index.js
@@ -0,0 +1,7 @@
+var data = {files:[
+{"link":"zcmd.zig.ba249d11.html","title":"zcmd.zig","summary_name":"[...]/zcmd/src/zcmd.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"173","uncovered_lines":"0","total_lines" : "173"},
+{"link":"panicTest.zig.a168240c.html","title":"panicTest.zig","summary_name":"[...]/zcmd/src/panicTest.zig","covered_class":"lineCov","covered":"100.0","covered_lines":"35","uncovered_lines":"0","total_lines" : "35"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "zcmd_kcov_test4", "date" : "2024-01-14 19:55:49", "instrumented" : 208, "covered" : 208,};
+var merged_data = [];
