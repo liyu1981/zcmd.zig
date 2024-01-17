d952ab2e532c30ab074f8c186587fe895d28efa7
Yu Li <liyu1981@gmail.com>
2024-01-18 10:26:21 +1100

    move back tests back to zcmd.zig

--- src/zcmd.zig
+++ src/zcmd.zig
@@ -34,23 +34,15 @@ pub const Term = union(enum) {
         else if (std.os.W.IFSIGNALED(status))
             Term{ .Signal = std.os.W.TERMSIG(status) }
         else if (std.os.W.IFSTOPPED(status))
-            unreachable
             // Term{ .Stopped = std.os.W.STOPSIG(status) }
+            unreachable
         else
+            // Term{ .Unknown = status };
             unreachable;
-        // Term{ .Unknown = status };
     }
 };
 
-pub const StdIo = enum {
-    Inherit,
-    Ignore,
-    Pipeline,
-    Pipe,
-    Close,
-};
-
-pub const ZCmdError = error{
+pub const ZcmdError = error{
     OutOfMemory,
 
     InputOutput,
@@ -64,29 +56,22 @@ pub const ZCmdError = error{
     SocketNotConnected,
     NetNameDeleted,
 
-    /// POSIX-only. `StdIo.Ignore` was selected and opening `/dev/null` returned ENODEV.
-    NoDevice,
+    Unexpected,
+    ProcessFdQuotaExceeded,
+    SystemFdQuotaExceeded,
+    SystemResources,
+    AccessDenied,
+    IsDir,
+};
 
-    /// Windows-only. One of:
-    /// * `cwd` was provided and it could not be re-encoded into UTF16LE, or
-    /// * The `PATH` or `PATHEXT` environment variable contained invalid UTF-8.
-    InvalidUtf8,
-
-    /// Windows-only. `cwd` was provided, but the path did not exist when spawning the child process.
-    CurrentWorkingDirectoryUnlinked,
-} ||
-    std.os.ExecveError ||
-    std.os.SetIdError ||
-    std.os.ChangeCurDirError;
-
-pub const SingleRunResult = struct {
+pub const RunResult = struct {
     allocator: std.mem.Allocator,
 
     term: Term = undefined,
     stdout: ?[]const u8 = null,
     stderr: ?[]const u8 = undefined,
 
-    pub fn deinit(this: *const SingleRunResult) void {
+    pub fn deinit(this: *const RunResult) void {
         if (this.stdout) |stdout_slice| this.allocator.free(stdout_slice);
         if (this.stderr) |stderr_slice| this.allocator.free(stderr_slice);
     }
@@ -94,40 +79,18 @@ pub const SingleRunResult = struct {
 
 const Zcmd = @This();
 
-const ErrInt = std.meta.Int(.unsigned, @sizeOf(anyerror) * 8);
+const ZcmdArgs = struct {
+    allocator: std.mem.Allocator,
+    commands: []const []const []const u8,
+    stdin_input: ?[]const u8 = null,
+    cwd: ?[]const u8 = null,
+    cwd_dir: ?std.fs.Dir = null,
+    env_map: ?*const std.process.EnvMap = null,
+    max_output_bytes: usize = MAX_OUTPUT,
+    expand_arg0: std.ChildProcess.Arg0Expand = .no_expand,
+};
 
-fn writeIntFd(fd: i32, value: ErrInt) !void {
-    const file = std.fs.File{
-        .handle = fd,
-        .capable_io_mode = .blocking,
-        .intended_io_mode = .blocking,
-    };
-    file.writer().writeInt(u64, @intCast(value), .little) catch return error.SystemResources;
-}
-
-fn readIntFd(fd: i32) !ErrInt {
-    const file = std.fs.File{
-        .handle = fd,
-        .capable_io_mode = .blocking,
-        .intended_io_mode = .blocking,
-    };
-    return @as(ErrInt, @intCast(file.reader().readInt(u64, .little) catch return error.SystemResources));
-}
-
-fn fifoToOwnedArrayList(fifo: *std.io.PollFifo) std.ArrayList(u8) {
-    if (fifo.head > 0) {
-        @memcpy(fifo.buf[0..fifo.count], fifo.buf[fifo.head..][0..fifo.count]);
-    }
-    const result = std.ArrayList(u8){
-        .items = fifo.buf[0..fifo.count],
-        .capacity = fifo.buf.len,
-        .allocator = fifo.allocator,
-    };
-    fifo.* = std.io.PollFifo.init(fifo.allocator);
-    return result;
-}
-
-pub fn run(allocator: std.mem.Allocator, commands: []const []const []const u8) ZCmdError!SingleRunResult {
+pub fn run(args: ZcmdArgs) ZcmdError!RunResult {
     const stdout_pipe = try std.os.pipe2(0);
     const stderr_pipe = try std.os.pipe2(0);
     const err_pipe = try std.os.pipe2(0);
@@ -137,26 +100,20 @@ pub fn run(allocator: std.mem.Allocator, commands: []const []const []const u8) Z
         std.os.close(err_pipe[0]);
         defer std.os.close(err_pipe[1]);
 
-        std.os.dup2(stdout_pipe[1], std.os.STDOUT_FILENO) catch |err| {
-            writeIntFd(err_pipe[1], @as(ErrInt, @intFromError(err))) catch {};
-        };
-        std.os.dup2(stderr_pipe[1], std.os.STDERR_FILENO) catch |err| {
-            writeIntFd(err_pipe[1], @as(ErrInt, @intFromError(err))) catch {};
-        };
+        std.os.dup2(stdout_pipe[1], std.os.STDOUT_FILENO) catch |err| forkChildErrReport(err_pipe[1], err);
+        std.os.dup2(stderr_pipe[1], std.os.STDERR_FILENO) catch |err| forkChildErrReport(err_pipe[1], err);
         std.os.close(stdout_pipe[0]);
         std.os.close(stdout_pipe[1]);
         std.os.close(stderr_pipe[0]);
         std.os.close(stderr_pipe[1]);
-        Zcmd._run(allocator, commands) catch |err| {
-            writeIntFd(err_pipe[1], @as(ErrInt, @intFromError(err))) catch {};
-        };
+        Zcmd._run(args) catch |err| forkChildErrReport(err_pipe[1], err);
         std.os.exit(0);
     } else {
         // we are parent
         std.os.close(stdout_pipe[1]);
         std.os.close(stderr_pipe[1]);
 
-        var poller = std.io.poll(allocator, enum { stdout, stderr }, .{
+        var poller = std.io.poll(args.allocator, enum { stdout, stderr }, .{
             .stdout = std.fs.File{ .handle = stdout_pipe[0] },
             .stderr = std.fs.File{ .handle = stderr_pipe[0] },
         });
@@ -172,15 +129,15 @@ pub fn run(allocator: std.mem.Allocator, commands: []const []const []const u8) Z
             std.os.close(err_pipe[1]);
         }
         if (err_int != std.math.maxInt(ErrInt)) {
-            return @as(ZCmdError, @errorCast(@errorFromInt(err_int)));
+            return @as(ZcmdError, @errorCast(@errorFromInt(err_int)));
         }
 
         // var stdout_f = std.fs.File{ .handle = stdout_pipe[0] };
         // // defer std.os.close(stdout_pipe[0]);
         // const out = try stdout_f.readToEndAlloc(allocator, MAX_OUTPUT);
         const result = std.os.waitpid(pid_result, 0);
-        return SingleRunResult{
-            .allocator = allocator,
+        return RunResult{
+            .allocator = args.allocator,
             .term = Term.fromStatus(result.status),
             .stdout = try stdout_array.toOwnedSlice(),
             .stderr = try stderr_array.toOwnedSlice(),
@@ -188,7 +145,7 @@ pub fn run(allocator: std.mem.Allocator, commands: []const []const []const u8) Z
     }
 }
 
-pub fn _run(allocator: std.mem.Allocator, commands: []const []const []const u8) !void {
+pub fn _run(args: ZcmdArgs) !void {
     // here we create a pipe then fork a copy of ourself, but instead of executing command, we do it in parent, and
     // let child to prepare for next environment. Using an example command pipelien
     // `cat ./tests/big_input.txt | wc -lw | wc-lw`, we will
@@ -197,12 +154,18 @@ pub fn _run(allocator: std.mem.Allocator, commands: []const []const []const u8) 
     //    then nothing so we get out of for loop)
     // the whole pipeline still use STDIN as input and STDOUT as output, so if we wrap this again, we can capture
     // the io streams
-    for (commands, 0..) |next_command, i| {
+    for (args.commands, 0..) |next_command, i| {
         const pipe_flags = 0;
         var pipe = try std.os.pipe2(pipe_flags);
         const pid_result = try std.os.fork();
         if (pid_result == 0) {
             // we are child
+            if (i + 1 == args.commands.len) {
+                // at the end just clean up and exit
+                std.os.close(pipe[0]);
+                std.os.close(pipe[1]);
+                std.os.exit(0);
+            }
             try std.os.dup2(pipe[0], std.os.STDIN_FILENO);
             std.os.close(pipe[0]);
             std.os.close(pipe[1]);
@@ -210,12 +173,13 @@ pub fn _run(allocator: std.mem.Allocator, commands: []const []const []const u8) 
         } else {
             // we are parent
             // std.debug.print("\nwill run command: {s}:{d}\n", .{ next_command, i });
-            if (i + 1 != commands.len) {
+            if (i + 1 != args.commands.len) {
                 try std.os.dup2(pipe[1], std.os.STDOUT_FILENO);
             }
+            // timing is critical, so do not use defer for closing the pipe
             std.os.close(pipe[0]);
             std.os.close(pipe[1]);
-            Zcmd.executeCommand(allocator, next_command) catch |err| {
+            Zcmd.executeCommand(args.allocator, next_command) catch |err| {
                 std.io.getStdErr().writer().print("zig: {any}: {s}\n", .{ err, next_command }) catch {};
                 std.os.exit(1);
             };
@@ -251,4 +215,143 @@ fn executeCommand(allocator: std.mem.Allocator, command: []const []const u8) !vo
         envp,
     );
     return exec_error;
+}
+
+const ErrInt = std.meta.Int(.unsigned, @sizeOf(anyerror) * 8);
+
+fn writeIntFd(fd: i32, value: ErrInt) !void {
+    const file = std.fs.File{
+        .handle = fd,
+        .capable_io_mode = .blocking,
+        .intended_io_mode = .blocking,
+    };
+    file.writer().writeInt(u64, @intCast(value), .little) catch return error.SystemResources;
+}
+
+fn readIntFd(fd: i32) !ErrInt {
+    const file = std.fs.File{
+        .handle = fd,
+        .capable_io_mode = .blocking,
+        .intended_io_mode = .blocking,
+    };
+    return @as(ErrInt, @intCast(file.reader().readInt(u64, .little) catch return error.SystemResources));
+}
+
+// Child of fork calls this to report an error to the fork parent.
+// Then the child exits.
+fn forkChildErrReport(fd: i32, err: ZcmdError) noreturn {
+    writeIntFd(fd, @as(ErrInt, @intFromError(err))) catch {};
+    // If we're linking libc, some naughty applications may have registered atexit handlers
+    // which we really do not want to run in the fork child. I caught LLVM doing this and
+    // it caused a deadlock instead of doing an exit syscall. In the words of Avril Lavigne,
+    // "Why'd you have to go and make things so complicated?"
+    if (builtin.link_libc) {
+        // The _exit(2) function does nothing but make the exit syscall, unlike exit(3)
+        std.c._exit(1);
+    }
+    std.os.exit(1);
+}
+
+fn fifoToOwnedArrayList(fifo: *std.io.PollFifo) std.ArrayList(u8) {
+    if (fifo.head > 0) {
+        @memcpy(fifo.buf[0..fifo.count], fifo.buf[fifo.head..][0..fifo.count]);
+    }
+    const result = std.ArrayList(u8){
+        .items = fifo.buf[0..fifo.count],
+        .capacity = fifo.buf.len,
+        .allocator = fifo.allocator,
+    };
+    fifo.* = std.io.PollFifo.init(fifo.allocator);
+    return result;
+}
+
+fn _testIsError(comptime T: type, maybe_value: anyerror!T, expected_error: anyerror) bool {
+    if (maybe_value) |_| {
+        return false;
+    } else |err| {
+        return err == expected_error;
+    }
+}
+
+test "normal cases" {
+    const allocator = std.testing.allocator;
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "uname", "-a" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqual(result.term.Exited, 0);
+        try testing.expect(result.stdout.?.len > 0);
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "cat", "./tests/big_input.txt" },
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(
+            u8,
+            result.stdout.?,
+            "    1302    2604\n",
+        );
+    }
+}
+
+test "all failures" {
+    const allocator = std.testing.allocator;
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "find", "tests" },
+                &.{ "sort-of", "-nr" },
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
+        try testing.expectEqualSlices(
+            u8,
+            result.stderr.?,
+            "zig: error.FileNotFound: { sort-of, -nr }\n",
+        );
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{"nonexist"},
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
+        try testing.expectEqualSlices(
+            u8,
+            result.stderr.?,
+            "zig: error.FileNotFound: { nonexist }\n",
+        );
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "find", "nonexist" },
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
+        try testing.expectEqualSlices(
+            u8,
+            result.stderr.?,
+            "find: nonexist: No such file or directory\n",
+        );
+    }
 }
--- src/zcmd_test.zig
+++ src/zcmd_test.zig
@@ -12,74 +12,97 @@ fn _testIsError(comptime T: type, maybe_value: anyerror!T, expected_error: anyer
     }
 }
 
-pub fn main() !u8 {
+test {
     const allocator = std.heap.page_allocator;
-    // {
-    //     const result = try Zcmd.run(allocator, &[_][]const []const u8{
-    //         &.{ "uname", "-a" },
-    //     });
-    //     defer result.deinit();
-    //     try testing.expectEqual(result.term.Exited, 0);
-    //     try testing.expect(result.stdout.?.len > 0);
-    // }
-    // {
-    //     const result = try Zcmd.run(allocator, &[_][]const []const u8{
-    //         &.{ "cat", "./tests/big_input.txt" },
-    //         &.{ "wc", "-lw" },
-    //         &.{ "wc", "-lw" },
-    //     });
-    //     defer result.deinit();
-    //     try testing.expectEqualSlices(u8, result.stdout.?, "       1       2\n");
-    // }
-    // {
-    //     const result = Zcmd.run(
-    //         allocator,
-    //         &[_][]const []const u8{
-    //             &.{"nonexist"},
-    //         },
-    //     );
-    //     try testing.expectError(error.FileNotFound, result);
-    // }
-    // {
-    //     const result = Zcmd.run(
-    //         allocator,
-    //         &[_][]const []const u8{
-    //             &.{ "find", "nonexist" },
-    //             &.{ "wc", "-lw" },
-    //         },
-    //     );
-    //     // std.debug.print("\n{any}\n", .{result});
-    //     try testing.expectError(error.FileNotFound, result);
-    // }
-    // {
-    //     try Zcmd._run(allocator, &[_][]const []const u8{
-    //         &.{ "find", "tests" },
-    //         &.{ "sort-of", "-nr" },
-    //         &.{ "wc", "-lw" },
-    //     });
-    // }
     {
-        const result = try Zcmd.run(allocator, &[_][]const []const u8{
-            &.{ "find", "tests" },
-            &.{ "sort-of", "-nr" },
-            &.{ "wc", "-lw" },
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "uname", "-a" },
+            },
         });
         defer result.deinit();
-        //std.debug.print("\nstdout: {?s}\nstderr: {?s}\n", .{ result.stdout, result.stderr });
-        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
-        try testing.expectEqualSlices(u8, result.stderr.?, "zig: error.FileNotFound: { sort-of, -nr }\n");
+        try testing.expectEqual(result.term.Exited, 0);
+        try testing.expect(result.stdout.?.len > 0);
     }
-    // {
-    //     try Zcmd._run(allocator, &[_][]const []const u8{
-    //         &.{"nonexist"},
-    //         &.{ "wc", "-lw" },
-    //     });
-    // }
-    // {
-    //     try Zcmd._run(allocator, &[_][]const []const u8{
-    //         &.{ "find", "nonexist" },
-    //         &.{ "wc", "-lw" },
-    //     });
-    // }
+}
+
+pub fn main() !u8 {
+    const allocator = std.heap.page_allocator;
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "uname", "-a" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqual(result.term.Exited, 0);
+        try testing.expect(result.stdout.?.len > 0);
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "cat", "./tests/big_input.txt" },
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(
+            u8,
+            result.stdout.?,
+            "    1302    2604\n",
+        );
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "find", "tests" },
+                &.{ "sort-of", "-nr" },
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
+        try testing.expectEqualSlices(
+            u8,
+            result.stderr.?,
+            "zig: error.FileNotFound: { sort-of, -nr }\n",
+        );
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{"nonexist"},
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
+        try testing.expectEqualSlices(
+            u8,
+            result.stderr.?,
+            "zig: error.FileNotFound: { nonexist }\n",
+        );
+    }
+    {
+        const result = try Zcmd.run(.{
+            .allocator = allocator,
+            .commands = &[_][]const []const u8{
+                &.{ "find", "nonexist" },
+                &.{ "wc", "-lw" },
+            },
+        });
+        defer result.deinit();
+        try testing.expectEqualSlices(u8, result.stdout.?, "       0       0\n");
+        try testing.expectEqualSlices(
+            u8,
+            result.stderr.?,
+            "find: nonexist: No such file or directory\n",
+        );
+    }
     return 0;
 }
